92d63b5a8e267acecb869ff7c9645a4e8099704f
maek <jak.malkowski@gmail.com>
2021-03-16 19:41:58 +0100

    Initializing project.

---
+++ src/main/java/io/stattrack/stattrack/StatTrackApplication.java
@@ -0,0 +1,15 @@
+package io.stattrack.stattrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatTrackApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(StatTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/stattrack/stattrack/StatTrackApplicationTests.java
@@ -0,0 +1,15 @@
+package io.stattrack.stattrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatTrackApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
