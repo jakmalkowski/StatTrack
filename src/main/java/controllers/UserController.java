d3d590527c9cd67274d3ad79535653e5b3cfe165
EnvyCy <cichyjan1@gmail.com>
2021-04-28 21:12:23 +0200

    Update UserController.java

--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -1,4 +1,14 @@
 package controllers;
 
+import models.UserService;
+
 public interface UserController {
+    public static void linkAccount(String game, String account) {}
+    public static void unlinkAccount(String game, String account) {}
+    public static void createUser(String login, String password, String email) {}
+    public static void logIn(String login, String password) {}
+    public static void logOut(){}
+    public static void userPage(){}
+    public static void settings(String someString) {}      //What String?
+    public static void mainPage() {}
 }
