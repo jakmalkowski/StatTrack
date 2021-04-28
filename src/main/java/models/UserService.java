0c420e68dec5a457333e4adeb153bec001f4f76d
sh4 <7331319+ShamerOC@users.noreply.github.com>
2021-04-28 21:07:24 +0200

    basic user service methods

--- src/main/java/models/UserService.java
+++ src/main/java/models/UserService.java
@@ -1,4 +1,21 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public interface UserService {
+
+    public void updateStats();
+
+    public void setDisplayed(HashMap<String, HashMap<String, ?>> args);
+
+    public void addToDisplayed(String gameName, ArrayList<String> arrayToAdd);
+
+    public void deleteFromDisplayed(String gameName, ArrayList<String> arrayToDelete);
+
+    public void setAccounts(HashMap<String, String> accounts);
+
+    public void linkAccount (String gameName, String account);
+
+    public void changeBio(String bio);
 }
--- src/main/java/models/UserServiceImpl.java
+++ src/main/java/models/UserServiceImpl.java
@@ -1,4 +1,49 @@
 package models;
 
-public class UserServiceImpl {
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class UserServiceImpl implements UserService {
+
+    UserModel userModel;
+
+    public UserServiceImpl (UserModel userModel) {
+        this.userModel = userModel;
+    }
+
+
+    @Override
+    public void updateStats() {
+
+    }
+
+    @Override
+    public void setDisplayed(HashMap<String, HashMap<String, ?>> args) {
+
+    }
+
+    @Override
+    public void addToDisplayed(String gameName, ArrayList<String> arrayToAdd) {
+
+    }
+
+    @Override
+    public void deleteFromDisplayed(String gameName, ArrayList<String> arrayToDelete) {
+
+    }
+
+    @Override
+    public void setAccounts(HashMap<String, String> accounts) {
+
+    }
+
+    @Override
+    public void linkAccount(String gameName, String account) {
+
+    }
+
+    @Override
+    public void changeBio(String bio) {
+
+    }
 }
