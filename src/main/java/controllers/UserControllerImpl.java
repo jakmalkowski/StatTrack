e93a861958f30ed4588df15d31e7fc4a620f78d6
EnvyCy <cichyjan1@gmail.com>
2021-04-28 21:03:36 +0200

    Update UserControllerImpl.java

--- src/main/java/controllers/UserControllerImpl.java
+++ src/main/java/controllers/UserControllerImpl.java
@@ -1,4 +1,17 @@
 package controllers;
 
-public class UserControllerImpl {
+import models.UserService;
+
+public class UserControllerImpl<UserModel> {
+    private UserService UserService;
+//    private UserView UserView;
+
+    public void linkAccount(String game, String account) {}
+    public void unlinkAccount(String game, String account) {}
+    public void createUser(String login, String password, String email) {}
+    public void logIn(String login, String password) {}
+    public void logOut(){}
+    public void userPage(){}
+    public void settings(String someString) {}      //What String?
+    public void mainPage() {}
 }
