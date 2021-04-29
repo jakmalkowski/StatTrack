6f96f177c38f699324c10a9819cafef84a430565
maek <jak.malkowski@gmail.com>
2021-04-29 19:14:30 +0200

    Added APIhandler Tests and improvements to the RiotAPIHandler itself

--- src/main/java/io/stattrack/stattrack/RiotApiHandler.java
+++ src/main/java/io/stattrack/stattrack/RiotApiHandler.java
@@ -1,16 +1,86 @@
 package io.stattrack.stattrack;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+import org.bson.json.JsonReader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
+
 //Pulls data from logged user Session, or gets them from a set String
 public class RiotApiHandler {
-    String region;
-    String summonerName;
-    String encryptedPUUId;
+    String region="";
+    String summonerName="";
+    String encryptedPUUID="";
+    ArrayList<String> last20games;
+    Map<String,String> matchdetails=Collections.emptyMap();
 
     RiotApiHandler(String region,String summonerName){
         this.region=region;
         this.summonerName=summonerName;
+        try
+        {
+            this.encryptedPUUID=getEncryptedPUUID(summonerName,region);
+        } catch (IOException exception)
+        {
+            exception.printStackTrace();
+        }
+        this.last20games=getMatchlist();
 
     }
-    String getPUUid(String summonerName,String region){
-        String
+    RiotApiHandler(){
+
+    }
+    String getEncryptedPUUID(String summonerName,String region) throws IOException
+    {
+        String json;
+        //Hardcoded for now, I'll set it up later, consists of https://{server}.api{...}/by-name/{SummonerName}{ApiKey}
+        String requestBase="https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+summonerName+"?api_key=RGAPI-2fb630ae-9481-4536-99a5-88b61ac1cc8c";
+        json=getGsonBase(requestBase);
+        Gson gson=new Gson();
+        Map<String,String> map=gson.fromJson(json,new TypeToken<Map<String,String>>(){}.getType());
+        return map.get("puuid");
+    }
+    String getMatchDetails(String matchID){
+        //Hardcoded for now
+        String requestBase ="https://europe.api.riotgames.com/lol/match/v5/matches/"+matchID+"?api_key=RGAPI-2fb630ae-9481-4536-99a5-88b61ac1cc8c";
+        JsonObject obj = new Gson().fromJson(getGsonBase(requestBase),JsonObject.class);
+        //Work in progress, for now returns the entire string dispatched by the API
+        //Map<Map<String,String>,String> matchDetails = gson.fromJson(json,new TypeToken<Map<Map<String,String>,String>>(){}.getType());
+        return obj.toString();
+    }
+    String getGsonBase(String requestBase){
+        try{
+            String json="";
+            URL url=new URL(requestBase);
+            URLConnection conn=url.openConnection();
+            conn.connect();
+            BufferedReader in= new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputline;
+            while((inputline=in.readLine())!=null){
+                json+=inputline;
+            }
+            return json;
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return "";
+    }
+    ArrayList<String> getMatchlist(){
+        String json;
+        String requestBase="https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/"+this.encryptedPUUID+"/ids?start=0&count=20&api_key=RGAPI-2fb630ae-9481-4536-99a5-88b61ac1cc8c";
+        json=getGsonBase(requestBase);
+        Gson gson=new Gson();
+        return gson.fromJson(json,new TypeToken<ArrayList<String>>(){}.getType());
     }
 }
--- src/test/java/io/stattrack/stattrack/ApiHandlingTests.java
+++ src/test/java/io/stattrack/stattrack/ApiHandlingTests.java
@@ -1,32 +1,102 @@
 package io.stattrack.stattrack;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ApiHandlingTests {
-    private List<String> tests;
-
-    @Before
-    public void init(){
-        tests = new ArrayList<>(Arrays.asList("test1","test2"));
-    }
 
     @Test
-    public void UserInfoTest(){
-
+    public void getUserEncryptedPUUID(){
+        RiotApiHandler userTest=new RiotApiHandler();
+        try {
+            String encryptedId = userTest.getEncryptedPUUID("8f9zu86yj87xgh76", "eun1");
+            Assert.assertEquals("ex0TUDyqcEumPfUFjmmTplmf6iHYdFnOi5HjjcFkz9byQNrCVdEdbaXm6CnpIbXS11iMKs8uNpXJ2Q",encryptedId);
+        }
+        catch (IOException exception) {
+            exception.printStackTrace();
+        }
     }
 
-    @After
-    public void cleanup(){
-        tests.clear();
+   @Test
+    public void getMatchDetails(){
+        try {
+            RiotApiHandler userTest = new RiotApiHandler("eun1", "8f9zu86yj87xgh76");
+            Path testfile = Path.of("/home/maek/StatTrack/src/test/java/io/stattrack/stattrack/matchinfo.txt");
+            String fin = Files.readString(testfile);
+            Assert.assertEquals(fin,userTest.getMatchDetails("EUN1_2775220260"));
+        }
+        catch (IOException exception){
+            exception.printStackTrace();
+        }
+   }
+    @Test
+    public void getMatchDetails2(){
+        try {
+            RiotApiHandler userTest = new RiotApiHandler("eun1", "Redlat");
+            Path testfile = Path.of("/home/maek/StatTrack/src/test/java/io/stattrack/stattrack/matchinfo.txt");
+            String fin = Files.readString(testfile);
+            Assert.assertNotEquals(fin,userTest.getMatchDetails("EUN1_2775220260"));
+        }
+        catch (IOException exception){
+            exception.printStackTrace();
+        }
     }
+   @Test
+    public void getMatchList(){
+        RiotApiHandler userTest= new RiotApiHandler("eun1","8f9zu86yj87xgh76");
+        String[] array= {"EUN1_2813356879", "EUN1_2797819662", "EUN1_2797712721", "EUN1_2797709856", "EUN1_2797693226", "EUN1_2797682448", "EUN1_2797526002",
+                "EUN1_2797504038", "EUN1_2797540836", "EUN1_2794696829", "EUN1_2791827193", "EUN1_2791781454", "EUN1_2791655029",
+                "EUN1_2791596648", "EUN1_2786187593", "EUN1_2785993952", "EUN1_2727858238", "EUN1_2727671015", "EUN1_2727312718", "EUN1_2727226847"
+        };
+        ArrayList<String> correct = new ArrayList<>(Arrays.asList(array));
+        Assert.assertEquals(correct ,userTest.getMatchlist());
+   }
+   @Test
+    public void getMatchList2(){
+        //Actually found a bug, handler doesn't parse the string correctly(in getGsonbase) if it contains whitespace
+        RiotApiHandler userTest= new RiotApiHandler("eun1","Stains of Time");
+        String[] array= {"EUN1_2775220260", "EUN1_2774180103", "EUN1_2773714308", "EUN1_2773662778", "EUN1_2772530238", "EUN1_2772505023",
+                "EUN1_2772491193", "EUN1_2772005021", "EUN1_2771803452", "EUN1_2769206534", "EUN1_2767311256", "EUN1_2767113653", "EUN1_2767101111",
+                "EUN1_2767036478", "EUN1_2766884534", "EUN1_2766808664", "EUN1_2766432042", "EUN1_2766071093", "EUN1_2766077729", "EUN1_2765668508"
+        };
+        ArrayList<String> correct = new ArrayList<>(Arrays.asList(array));
+        Assert.assertEquals(correct , userTest.getMatchlist());
+   }
+   @Test
+    public void Combined(){
+        try
+        {
+            RiotApiHandler userTest = new RiotApiHandler("eun1", "Redlat");
+            String[] array = {"EUN1_2775220260", "EUN1_2774180103", "EUN1_2773714308", "EUN1_2773662778", "EUN1_2772530238", "EUN1_2772505023",
+                    "EUN1_2772491193", "EUN1_2772005021", "EUN1_2771803452", "EUN1_2769206534", "EUN1_2767311256", "EUN1_2767113653", "EUN1_2767101111",
+                    "EUN1_2767036478", "EUN1_2766884534", "EUN1_2766808664", "EUN1_2766432042", "EUN1_2766071093", "EUN1_2766077729", "EUN1_2765668508"
+            };
+            ArrayList<String> correct = new ArrayList<>(Arrays.asList(array));
+            Path testfile = Path.of("/home/maek/StatTrack/src/test/java/io/stattrack/stattrack/matchinfo2.txt");
+            String fin = Files.readString(testfile);
+            //pUUID test
+            Assert.assertEquals("pkJUgeuYD1wH6LqRsGmUYATe3F1T3NltD_s2wkDTzgKG8wdF4dABME5lH4yN05LAgCjzdg18BCmp5w",userTest.getEncryptedPUUID(userTest.summonerName, userTest.region));
+            //matchList test
+            Assert.assertEquals(correct,userTest.getMatchlist());
+            //matchDetails test
+            Assert.assertEquals(fin,userTest.getMatchDetails("EUN1_2772505023"));
+        } catch (IOException exception)
+        {
+            exception.printStackTrace();
+        }
+
+   }
 }
